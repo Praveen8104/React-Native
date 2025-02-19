13ef2646b0d242695aa831a78cf5a443da803d4e
Praveen8104 <praveenreddygoi8@gmail.com>
2025-02-19 11:18:34 +0530

    add image sharing functionality and update dependencies

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import Carousel from './Components/carousel';
 import animation from './Components/animation';
 import bouningball from './Components/bouningball';
 import imagepicker from './Components/imagepicker';
+import imageSharing from './Components/imagesharing';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
@@ -17,5 +18,6 @@ import imagepicker from './Components/imagepicker';
 // registerRootComponent(Carousel);
 // registerRootComponent(animation);
 // registerRootComponent(bouningball);
-registerRootComponent(imagepicker)
+// registerRootComponent(imagepicker)
+registerRootComponent(imageSharing)
 
