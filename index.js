f3f5b312d3439acfde59f18cf7ed4faacd30bd88
Praveen8104 <praveenreddygoi8@gmail.com>
2025-02-20 10:24:35 +0530

    add image sharing component and update expo-file-system version

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import Carousel from './Components/carousel';
 import animation from './Components/animation';
 import bouningball from './Components/bouningball';
 import imagepicker from './Components/imagepicker';
+import imageSharing from './Components/imagesharing';
 import cameraaccess from './Components/cameraaccess';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
@@ -19,5 +20,5 @@ import cameraaccess from './Components/cameraaccess';
 // registerRootComponent(animation);
 // registerRootComponent(bouningball);
 // registerRootComponent(imagepicker)
-// registerRootComponent(imageSharing)
-registerRootComponent(cameraaccess);
+registerRootComponent(imageSharing)
+// registerRootComponent(cameraaccess);
